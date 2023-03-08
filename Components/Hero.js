12463511a0bbde29a289d54b5ff9de3e385b7249
glennanj1 <glennan20@gmail.com>
2023-03-07 22:47:35 -0500

    trigger deploy

--- Components/Hero.js
+++ Components/Hero.js
@@ -35,7 +35,7 @@ function HeroHome() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Blyssfull <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Magick</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Start your spiritual journey here!</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Start your spiritual journey here</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                 <button className="flex justify-center items-center content-center bg-blue-500 hover:bg-amber-500 text-white font-bold py-2 px-4 rounded">
