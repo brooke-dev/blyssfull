c0efa13c0b8cdee6d00677cad419a258a3f8d037
glennanj1 <glennan20@gmail.com>
2023-03-07 22:54:12 -0500

    tailwind config ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   important: true,
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.js",
+    "./pages/**/*.js",
+    "./Components/**/*.js",
+    "./Utils/**/*.js",
   ],
   theme: {
     extend: {
