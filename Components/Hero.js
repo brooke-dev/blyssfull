9063571796f0bb0ab59e961a3f9bfd91604da446
glennanj1 <glennan20@gmail.com>
2023-03-07 20:08:34 -0500

    removed spacing

--- Components/Hero.js
+++ Components/Hero.js
@@ -9,7 +9,6 @@ function HeroHome() {
 
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
