c2c8ecdc09ccdec55b456581642c8c1e8970c048
glennanj1 <glennan20@gmail.com>
2023-03-10 00:55:38 -0500

    coming soon?

---
+++ pages/book.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Book() {
+  return (
+    <div className="w-screen flex-auto justify-center align-center">
+        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+        Booking Coming Soon!
+        </h1>
+    </div>
+  );
+}
