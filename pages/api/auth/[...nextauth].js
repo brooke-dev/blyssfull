6a37868c4e7edad168bf71f27a83b3d07fc8bf2e
glennanj1 <glennan20@gmail.com>
2023-03-10 00:56:16 -0500

    email provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/signin",
+    error: "/error"
+  },
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, account, token }) {
+      session.userId = user.id;  
+      session.status = user.status;
+      return session
+    },
+  },
+})
