e2908b32c070f7d2fafbc4b340e6dcb589a543fc
glennanj1 <glennan20@gmail.com>
2023-03-12 22:53:35 -0400

    added session check to booking

--- pages/book.js
+++ pages/book.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function Book() {
-  return (
+    const { data: session } = useSession();
+    const router = useRouter()
+
+    useEffect(() => {
+      if (!session) router.push('/auth/signin/');
+    }, [router, session])
+    
+    return (
     <div className="w-screen flex-auto justify-center align-center">
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
         Booking Coming Soon!
