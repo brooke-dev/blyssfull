a510b186cbe2d68292f1395c802fd40a9adbf24f
glennanj1 <glennan20@gmail.com>
2023-03-12 22:53:01 -0400

    aos animation init

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import Header from '@/Components/Header';
+import AOS from 'aos';
+
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+    useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
