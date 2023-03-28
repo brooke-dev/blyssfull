e7dc2dd475211496555237ababc26102f0362e66
glennanj1 <glennan20@gmail.com>
2023-03-28 18:10:32 -0400

    color scheme update

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -73,11 +73,11 @@ export default function signin({ csrfToken, providers }) {
             <div>
               <button
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="group relative flex w-full justify-center rounded-md bg-purple-700 py-2 px-3 text-sm font-semibold text-white hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    className="h-5 w-5 text-purple-500 group-hover:text-purple-400"
                     aria-hidden="true"
                   />
                 </span>
