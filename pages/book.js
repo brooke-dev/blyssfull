26bb32126c4c0f3c2f9df64e081cab7ac1505749
glennanj1 <glennan20@gmail.com>
2023-03-29 09:12:11 -0400

    added intro session to options

--- pages/book.js
+++ pages/book.js
@@ -109,6 +109,9 @@ export default function Book() {
                             className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           >
                             <option value=""></option>
+                            <option value="15.00">
+                              Intoductory Session
+                            </option>
                             <option value="111.00">
                               1 Hour Distance Reiki Session
                             </option>
