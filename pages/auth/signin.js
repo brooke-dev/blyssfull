8c147276e1cd897550d2a4842b43ec175ba18569
glennanj1 <glennan20@gmail.com>
2023-04-05 23:47:29 -0400

    sign in updates to prevent button click more then once

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,18 +3,45 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { getCsrfToken } from "next-auth/react";
 import { LockClosedIcon } from "@heroicons/react/20/solid";
-import { useState } from 'react'
-import styles from '../../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from 'next/router'
 
 export default function signin({ csrfToken, providers }) {
+  const router = useRouter()
+  let [submitDisabled, setSubmitDisabled] = useState(false);
+  let [email, setEmail] = useState();
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("button clicked >> + button state " + submitDisabled);
+    setSubmitDisabled(true);
+    debugger;
+    fetch('/api/auth/signin/email/', {
+      method: 'POST',
+      body: JSON.stringify({csrfToken, email}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(response => {
+      console.log(response);
+      router.push('/auth/verifyrequest/')
+    }).catch(e => {
+      console.log(e);
+    });
+  };
+  let handleChange = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  }
+
   return (
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
             <svg
-              className='mx-auto h-12 w-auto'
+              className="mx-auto h-12 w-auto"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -48,23 +75,23 @@ export default function signin({ csrfToken, providers }) {
           </div>
           <form
             className="mt-8 space-y-6"
-            method="post"
-            action="/api/auth/signin/email"
+            onSubmit={handleSubmit}
           >
             <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
             <div className="-space-y-px rounded-md shadow-sm">
               <label htmlFor="email-address" className="sr-only">
                 Email address
               </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  autoComplete="email"
-                  required
-                  placeholder="Email address"
-                  className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+                required
+                placeholder="Email address"
+                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
               <div>
                 {/* <input id="email-address" name="email" type="email" /> */}
               </div>
@@ -73,6 +100,7 @@ export default function signin({ csrfToken, providers }) {
             <div>
               <button
                 type="submit"
+                disabled={submitDisabled}
                 className="group relative flex w-full justify-center rounded-md bg-purple-700 py-2 px-3 text-sm font-semibold text-white hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -86,17 +114,14 @@ export default function signin({ csrfToken, providers }) {
             </div>
           </form>
           {Object.values(providers).map((provider) => {
-            provider !== 'email' ?
-              (
-                <div key={provider.name}>
-                  <button onClick={() => signIn(provider.id)}>
-                    Sign in with {provider.name}
-                  </button>
-                </div>
-              ) : null
-            }
-            )
-          }
+            provider !== "email" ? (
+              <div key={provider.name}>
+                <button onClick={() => signIn(provider.id)}>
+                  Sign in with {provider.name}
+                </button>
+              </div>
+            ) : null;
+          })}
         </div>
       </div>
     </>
