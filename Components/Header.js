0e369bdcfc696ebc694fc9741ff6c274dd53a47a
glennanj1 <glennan20@gmail.com>
2023-04-06 00:14:12 -0400

    Profile page

--- Components/Header.js
+++ Components/Header.js
@@ -122,6 +122,11 @@ function Header({isBooking}) {
             {/* Site navigation */}
             <nav className="flex flex-grow hidden md:block">
               <ul className="flex flex-grow justify-end flex-wrap items-center">
+              <li>
+                <Link href='/Profile' className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                  Profile
+                </Link>
+              </li>
               {tabs.map(t => {
                 return (
                   <li key={t.id}>
