52a441487b2eed028410c79c0e0f1fcda5d94a08
glennanj1 <glennan20@gmail.com>
2023-04-05 23:46:14 -0400

    call back logic when first time user

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,17 @@
-import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "../../../lib/mongodb"
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI);
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   pages: {
     signIn: "/auth/signin",
     error: "/auth/error",
-    newUser: '/auth/newUser' // New users will be directed here on first sign in (leave the property out if not of interest)
+    verifyRequest: "/auth/verifyrequest",
+    newUser: "/auth/newUser", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   providers: [
     // GithubProvider({
@@ -23,9 +26,27 @@ export default NextAuth({
   ],
   callbacks: {
     async session({ session, user, account, token }) {
-      session.userId = user.id;  
+      console.log(user);
+      let User = mongoose.connection.db
+        .collection("users")
+        .findOne({ _id: user.id }, (err, user) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("found someone ayoooo");
+        });
+
+      if (User?.newUser) {
+        console.log("logging user >>>>>" + User);
+      } else {
+        await mongoose.connection.db
+          .collection("users")
+          .updateOne({ id: User.id }, { $set: { newUser: true } });
+      }
       session.status = user.status;
-      return session
+      session.newUser = user.newUser;
+      return session;
     },
   },
-})
+});
