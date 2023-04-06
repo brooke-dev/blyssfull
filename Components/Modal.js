9346abe44a3d93114fa41b373192f69cba7909fc
glennanj1 <glennan20@gmail.com>
2023-04-06 00:11:52 -0400

    modal fix

--- Components/Modal.js
+++ Components/Modal.js
@@ -80,40 +80,39 @@ export default function Modal({ isOpen, initiateReset, navItems, signedIn }) {
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   {tabs.map((t) => {
                     return (
-                      <button
+                      <Link
                         key={t.id}
-                        type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
-                        onClick={() => {
-                          setOpen(false);
-                          reset();
-                        }}
+                        href={t.value}
+                        className="font-medium hover:text-gray-900 flex items-center transition duration-150 ease-in-out"
                       >
-                        <Link
-                          href={t.value}
-                          className="font-medium hover:text-gray-900 flex items-center transition duration-150 ease-in-out"
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
+                          onClick={() => {
+                            setOpen(false);
+                            reset();
+                          }}
                         >
                           {t.label}
-                        </Link>
-                      </button>
+                        </button>
+                      </Link>
                     );
                   })}
                   {isSignedIn ? (
                     <>
-                      <button
-                        type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
-                        onClick={() => {
-                          setOpen(false);
-                          reset();
-                        }}
-                      >
-                      <Link
-                        href='/Profile'
-                      >
-                        Profile
+                      <Link href="/Profile">
+                        <button
+                          type="button"
+                          href="/Profile"
+                          className="mt-3 inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
+                          onClick={() => {
+                            setOpen(false);
+                            reset();
+                          }}
+                        >
+                          Profile
+                        </button>
                       </Link>
-                      </button>
                       <button
                         type="button"
                         className="mt-3 inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
