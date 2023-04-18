3832ec96ec63cbfdd0c5cf6ef7ead26a19bbe4bb
glennanj1 <glennan20@gmail.com>
2023-04-17 23:47:15 -0400

    fixed loading pulse and removed fields

--- pages/book.js
+++ pages/book.js
@@ -7,30 +7,26 @@ import Footer from "@/Components/Footer";
 import CustomDatePicker from "@/Components/CustomDatePicker";
 
 export default function Book() {
-  const today = new Date();
   const { data: session, status } = useSession();
   const router = useRouter();
   const [authed, setAuthed] = useState(false);
-  const [price, setPrice] = useState();
+  const [price, setPrice] = useState(null);
   const [desc, setDesc] = useState();
-  const [nextSelected, setNextSelected] = useState(false);
-  const [selectedDate, setSelectedDate] = useState("");
-
-  useEffect(() => {
-    console.log(price);
-    debugger;
-  }, [price]);
-
-  const handleClick = () => {
-    //setNextSelected(true);
-    //router.push('/Checkout')
-    console.log('clicked')
-    debugger;
-  };
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [formValid, setFormValid] = useState(true);
 
   const handleDateChange = (date) => {
-    console.log("Selected date:", date);
+    setSelectedDate(date);
   };
+
+  useEffect(() => {
+    if (selectedDate != null && price != null) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [selectedDate, price, setFormValid, formValid])
+  
 
   const handleServiceChange = (e) => {
     e.preventDefault();
@@ -45,60 +41,12 @@ export default function Book() {
         {/* <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
         Book
         </h1> */}
-        {nextSelected && price ? (
           <div class="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
             <div>
               <div class="md:grid md:grid-cols-3 md:gap-6">
                 <div class="md:col-span-1">
-                  <div class="px-4 sm:px-0">
-                    <h2 className="break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
-                      Book{" "}
-                      <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
-                        Here
-                      </span>
-                    </h2>
-                    {price ? (
-                      <h2 className="text-1xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4">
-                        Price: ${price}
-                      </h2>
-                    ) : null}
-                    <p class="mt-1 text-sm text-gray-600">
-                      Enter Some Details & Select a Service
-                    </p>
-                  </div>
-                </div>
-                <div class="mt-5 md:col-span-2 md:mt-0">
-                  <div className="overflow-hidden shadow sm:rounded-md">
-                    <div className="bg-white px-4 py-5 sm:p-6">
-                      <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                          {price ? (
-                            <Paypal cost={price} />
-                          ) : (
-                            <Paypal disabled="true" />
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => setNextSelected(false)}
-                    type="button"
-                    className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Back
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div class="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
-            <div>
-              <div class="md:grid md:grid-cols-3 md:gap-6">
-                <div class="md:col-span-1">
-                  <div class="px-4 sm:px-0">
-                    <h2 className="break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
+                  <div class="px-4 sm:px-4">
+                    <h2 className="lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
                       Book{" "}
                       <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
                         Here
@@ -119,7 +67,6 @@ export default function Book() {
                     <div className="overflow-hidden shadow sm:rounded-md">
                       <div className="bg-white px-4 py-5 sm:p-6">
                         <div className="grid grid-cols-6 gap-6">
-                          <div className="col-span-6 sm:col-span-3">
                             {/* <label
                               htmlFor="first-name"
                               className="block text-sm font-medium leading-6 text-gray-900"
@@ -172,7 +119,7 @@ export default function Book() {
                             />
                           </div> */}
 
-                            <div className="col-span-6 sm:col-span-3">
+                            <div className="col-span-6 sm:col-start-1 col-end-7">
                               <label
                                 htmlFor="services"
                                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -277,16 +224,16 @@ export default function Book() {
                               className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                           </div> */}
-                            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                            <div className="col-span-6 sm:col-start-1 col-end-7">
                               <label className="block text-sm font-medium leading-6 text-gray-900">
                                 Pick Date
                               </label>
                               <CustomDatePicker onChange={handleDateChange} />
                             </div>
-                          </div>
+                      
                         </div>
-                        <div onClick={handleClick} className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                            <Paypal buttonClicked={handleClick} isDisabled={true} />
+                        <div className="bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
+                            <Paypal isDisabled={formValid} cost={price} />
                         </div>
                       </div>
                     </div>
@@ -295,7 +242,6 @@ export default function Book() {
               </div>
             </div>
           </div>
-        )}
         <Footer />
       </>
     );
@@ -315,46 +261,58 @@ export default function Book() {
     <>
       <Header isBooking={true} />
       <div class="animate-pulse md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
-        <div>
-          <div class="md:grid md:grid-cols-3 md:gap-6">
-            <div class="md:col-span-1">
-              <div class="px-4 sm:px-0">
-                <div className="h-20 bg-slate-700 rounded break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
-                  {" "}
-                  <span className="bg-slate-700 h-10 w-10"></span>
+            <div>
+              <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1">
+                  <div class="px-4 sm:px-4">
+                    <h2 className="h-40 bg-slate-700 rounded lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
+                     
+                      <span className="h-20 bg-slate-700 rounded bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
+                      
+                      </span>
+                    </h2>
+                    <p class="h-10 bg-slate-700 rounded mt-1 text-sm text-gray-600">
+                    </p>
+                  </div>
                 </div>
+                <div class="mt-5 md:col-span-2 md:mt-0">
+                  <form>
+                    <div className="overflow-hidden shadow sm:rounded-md">
+                      <div className="bg-white px-4 py-5 sm:p-1">
+                        <div className="grid grid-cols-6 gap-6">
+                            <div className="col-span-6 sm:col-start-1 col-end-7">
+                              <label
+                                htmlFor="services"
+                                className="h-5 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                
+                              </label>
+                              <label
+                                htmlFor="services"
+                                className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                
+                              </label>
+                            
+                            </div>
 
-                <p class="h-5 bg-slate-700 rounded mt-1 text-sm text-gray-600"></p>
+                            <div className="col-span-6 sm:col-start-1 col-end-7">
+                              <label className="h-5 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900">
+                              </label>
+                              <label className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900">
+                              </label>
+                            </div>
+                      
+                        </div>
+                        <div className="h-20 bg-slate-700 rounded bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
-            <div class="mt-5 md:col-span-2 md:mt-0">
-              <form action="#" method="POST">
-                <div className="overflow-hidden sm:rounded-md">
-                  <div className="bg-white px-4">
-                    <div className="grid grid-cols-6 gap-6">
-                      <div className="h-10 bg-slate-700 rounded col-span-6 sm:col-span-3"></div>
-
-                      <div className="h-10 bg-slate-700 rounded col-span-6 sm:col-span-3"></div>
-
-                      <div className="h-10 bg-slate-700 rounded col-span-6 sm:col-span-4"></div>
-
-                      <div className="bg-slate-700 rounded col-span-6 sm:col-span-3"></div>
-
-                      <div className="h-10 bg-slate-700 rounded col-span-6 sm:col-span-3 lg:col-span-2"></div>
-                      <div className="h-10 bg-slate-700 rounded col-span-6 sm:col-span-3 lg:col-span-2"></div>
-                    </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <div className="h-10 mt-10 bg-slate-700 rounded col-span-6 sm:col-span-4"></div>
-                    <div className="h-10 mt-10 bg-slate-700 rounded col-span-6 sm:col-span-4"></div>
-                    <div className="h-10 mt-10 bg-slate-700 rounded col-span-6 sm:col-span-4"></div>
-                  </div>
-                </div>
-              </form>
-            </div>
           </div>
-        </div>
-      </div>
     </>
   );
 }
