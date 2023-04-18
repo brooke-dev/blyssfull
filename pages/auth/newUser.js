dfd9d1b84705aa60e32875d51d24e7b2564a9fac
glennanj1 <glennan20@gmail.com>
2023-04-17 20:48:15 -0400

    new user logic

--- pages/auth/newUser.js
+++ pages/auth/newUser.js
@@ -1,30 +1,73 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { useSession, useEffect } from "next-auth/react";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
+import { useRouter } from "next/router";
 
 export default function Welcome() {
   const [authed, setAuthed] = useState(false);
-  const { data: session, status, user} = useSession();
- 
-  
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const { data: session, status, user } = useSession();
+  const router = useRouter();
+
+  const reloadSession = () => {
+    const event = new Event("visibilitychange");
+    document.dispatchEvent(event);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    debugger;
+    fetch("/api/newuser/", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+      })
+    })
+      .then((res) => {
+        debugger;
+        if (res.ok) {
+          console.log("res ok");
+          debugger;
+          reloadSession();
+          router.push("/");
+        } else {
+          debugger;
+          console.log("Error please report email john");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   if (authed) {
     return (
       <>
-        <Header isBooking={true}/>
+        <Header isBooking={true} />
         <div class="md:shadow-xl pt-32 pb-12 md:p-80 md:pt-40 md:pb-20 w-50">
           <div>
             <div class="md:grid md:grid-cols-3 md:gap-6">
               <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
-                <h2 className="break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">Welcome 👋 <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">{session.user.email}</span></h2>
+                  <h2 className="break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
+                    Welcome 👋{" "}
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
+                      {session.user.email}
+                    </span>
+                  </h2>
                   <p class="mt-1 text-sm text-gray-600">
-                    Please Enter Some Details 
+                    Please Enter Some Details
                   </p>
                 </div>
               </div>
               <div class="mt-5 md:col-span-2 md:mt-0">
-                <form action="#" method="POST">
+                <form onSubmit={handleSubmit}>
                   <div className="overflow-hidden shadow sm:rounded-md">
                     <div className="bg-white px-4 py-5 sm:p-6">
                       <div className="grid grid-cols-6 gap-6">
@@ -36,10 +79,14 @@ export default function Welcome() {
                             First name
                           </label>
                           <input
+                            required
                             type="text"
                             name="first-name"
                             id="first-name"
                             autoComplete="given-name"
+                            onChange={(event) =>
+                              setFirstName(event.target.value)
+                            }
                             className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           />
                         </div>
@@ -52,10 +99,14 @@ export default function Welcome() {
                             Last name
                           </label>
                           <input
+                            required
                             type="text"
                             name="last-name"
                             id="last-name"
                             autoComplete="family-name"
+                            onChange={(event) =>
+                              setLastName(event.target.value)
+                            }
                             className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           />
                         </div>
@@ -80,55 +131,51 @@ export default function Welcome() {
     );
   }
   if (status === "authenticated") {
+    if (session.newUser === false) {
+      router.push('/')
+    }
     setTimeout(() => {
       setAuthed(true);
     }, 2000);
   }
-
   if (status === "unauthenticated") {
     router.push("/auth/Signin/");
   }
 
   return (
     <>
-    <Header isBooking={true}/>
-    <div class="animate-pulse md:shadow-xl pt-32 pb-12 md:p-80 md:pt-40 md:pb-20 w-50">
-      <div>
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1">
-            <div class="px-4 sm:px-0">
-            <h2 className="bg-slate-700 h-20 rounded break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"></h2>
+      <Header isBooking={true} />
+      <div class="animate-pulse md:shadow-xl pt-32 pb-12 md:p-80 md:pt-40 md:pb-20 w-50">
+        <div>
+          <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+              <div class="px-4 sm:px-0">
+                <h2 className="bg-slate-700 h-20 rounded break-words text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"></h2>
+              </div>
             </div>
-          </div>
-          <div class="mt-5 md:col-span-2 md:mt-0">
-            <form action="#" method="POST">
-              <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="bg-white px-4 py-5 sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="bg-slate-700 h-10 rounded col-span-6 sm:col-span-3">
-               
-                    </div>
+            <div class="mt-5 md:col-span-2 md:mt-0">
+              <form action="#" method="POST">
+                <div className="overflow-hidden shadow sm:rounded-md">
+                  <div className="bg-white px-4 py-5 sm:p-6">
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="bg-slate-700 h-10 rounded col-span-6 sm:col-span-3"></div>
 
-                    <div className="bg-slate-700 h-10 rounded col-span-6 sm:col-span-3">
-                   
+                      <div className="bg-slate-700 h-10 rounded col-span-6 sm:col-span-3"></div>
                     </div>
                   </div>
+                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="bg-slate-700 rounded inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    ></button>
+                  </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="bg-slate-700 rounded inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
-  </>
+      <Footer />
+    </>
   );
 }
