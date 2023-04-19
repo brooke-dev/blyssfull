65695a852c9888546189746e67b11c4b2bab3cd2
glennanj1 <glennan20@gmail.com>
2023-04-19 01:51:20 -0400

    backend index for post and get so far

--- pages/api/booking/index.js
+++ pages/api/booking/index.js
@@ -1,27 +1,36 @@
-import mongoose from 'mongoose';
-import Transaction from '../../../Models/Transaction.js'; // Path to your transaction model
-
+import mongoose from "mongoose";
+import Transaction from "../../../Models/Transaction.js"; // Path to your transaction model
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
 // Connect to your MongoDB database
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 async function handler(req, res) {
-  if (req.method === 'POST') {
-    const transactionData = req.body;
+  const session = await getServerSession(req, res, authOptions)
 
-    try {
-      const newTransaction = new Transaction(transactionData);
-      const savedTransaction = await newTransaction.save();
-      res.status(200).json(savedTransaction);
-    } catch (error) {
-      res.status(500).json({ message: 'Failed to save transaction', error });
+  if (session) {
+    if (req.method === "POST") {
+      const transactionData = req.body;
+      try {
+        const newTransaction = new Transaction(transactionData);
+        const savedTransaction = await newTransaction.save();
+        res.status(200).json(savedTransaction._id);
+      } catch (error) {
+        res.status(500).json({ message: "Failed to save transaction", error });
+      }
+    } else if (req.method === "GET") {
+      const bookings = await Transaction.find({ userId: session.id });
+          return res.status(200).json({ success: true, data: bookings }); // Added return statement
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: "Not authorized" });
   }
 }
 
