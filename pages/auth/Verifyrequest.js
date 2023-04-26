547f60956a8bf8fa9f0d2ef227f06ed7314a5394
John Glennan <glennan20@gmail.com>
2023-04-26 18:37:07 +0000

    push home on button click

--- pages/auth/Verifyrequest.js
+++ pages/auth/Verifyrequest.js
@@ -1,6 +1,8 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 export default function Verifyrequest() {
   const [emailSent, setEmailSent] = useState();
+  const router = useRouter();
 
   useEffect(() => {
     setTimeout(() => {
@@ -47,6 +49,7 @@ export default function Verifyrequest() {
               Email Sent!
             </button>
             <button
+              onClick={() => router.push('/')}
               disabled
               className="group relative flex w-full justify-center rounded-md bg-purple-700 py-2 px-3 text-sm font-semibold text-white hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
